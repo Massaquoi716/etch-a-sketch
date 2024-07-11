908a8ff687a1d5cdb3f863b408899a3bdb4a2f14
Massaquoi L. Dehmie <massaquoi2001@gmail.com>
2024-07-11 15:00:49 +0000

    add events listener to change grid background color when hovered over

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const containerDiv = document.querySelector(".container");
 
 //creates and populates containDiv with child divs with their width and heights provided by the user
 function createDivs(numDivs) {
-    for (let i = 1; i <= numDivs; i++) {
+    let grid = numDivs * numDivs;
+    for (let i = 1; i <= grid; i++) {
         // Create a new div element
         const newDiv = document.createElement('div');
 
         // Add a class to the new div 
         newDiv.classList.add('grid-item');
-        newDiv.style.backgroundColor = "blue";
+        
 
         // Append the new div to the container
         containerDiv.appendChild(newDiv);
@@ -17,3 +18,15 @@ function createDivs(numDivs) {
 
 // Create 16 divs
 createDivs(16);
+
+containerDiv.addEventListener("mouseover", (event) => {
+        // Generate random RGB values
+    let r = Math.floor(Math.random() * 256); // Random integer from 0 to 255
+    let g = Math.floor(Math.random() * 256); // Random integer from 0 to 255
+    let b = Math.floor(Math.random() * 256); // Random integer from 0 to 255
+    
+     // Construct RGB color string
+    let rgbColor = `rgb(${r}, ${g}, ${b})`;
+
+    event.target.style.backgroundColor = rgbColor;
+})
